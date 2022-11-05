7dfbdf7a73bd96d3cb54c8506bd4602766d4bb58
ArtemMihailov <lavash.karica@mail.ru>
2022-11-05 12:32:48 +0300

    upd

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,48 @@
 <template>
-    <div>
-        <b-row class="m-0 text-center pt-3">
-            <b-col class="p-0" cols="1">
-                <nuxt-link to="/">Логотип</nuxt-link>
-            </b-col>
-        </b-row>
+    <div class="header">
+        <b-navbar toggleable="lg" type="dark" variant="dark">
+            <b-navbar-brand href="#">LOGO</b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+                <b-col class="p-0 px-2 item">
+                    <b-img center src="@/assets/img/chip.png"></b-img>
+                    <div class="text text-center">ML</div>
+                </b-col>
+                <b-col class="p-0 px-2 item">
+                    <b-img center src="@/assets/img/docs.png"></b-img>
+                    <div class="text text-center">Исследования</div>
+                </b-col>
+                <b-col class="p-0 px-2 item">
+                    <b-img center src="@/assets/img/user.png"></b-img>
+                    <div class="text text-center">xxxx_xxxx</div>
+                </b-col>
+            </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
     </div>
 </template>
+
+<style scoped>
+    .header{
+        color: rgb(209, 209, 209);
+    }
+    img{
+        width: 30px;
+        height: 30px;
+        filter: grayscale(1);
+    }
+    .item{
+        cursor: pointer;
+    }
+    .item:hover .text{
+        color: #30d5c8;
+    }
+    .item:hover img{
+        filter: grayscale(0);
+    }
+</style>
