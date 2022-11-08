69cbc3c4d5135a749d7e990600171aeef479c955
ArtemMihailov <lavash.karica@mail.ru>
2022-11-08 19:48:45 +0300

    Update PictureBlock.vue

--- components/PictureBlock.vue
+++ components/PictureBlock.vue
@@ -12,7 +12,7 @@
                 <b-col cols="4" v-for="item in patient.pictures" :key="item.id" class="p-3 img-item">
                     <b-row class="m-0" align-h="center">
                         <Canvas :img="item.pict_prefix + item.pict" />
-                        <div class="pt-3" style="width: 500px">
+                        <div style="width: 500px">
                             <div class="property p-1"><b>Тип груди:</b> {{item.pict_property.selectedBreastType == 'left' ? 'Левая':'Правая'}}</div>
                             <div class="property p-1"><b>Разрешение:</b> {{item.pict_property.resolutionW}}x{{item.pict_property.resolutionH}} {{item.pict_property.selectedResolution}}</div>
                             <div class="property p-1"><b>Аппроксимация:</b> {{item.pict_property.approximationW}}x{{item.pict_property.approximationH}} {{item.pict_property.selectedApproximation}}</div>
